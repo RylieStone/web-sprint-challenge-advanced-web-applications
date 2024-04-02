9edc4cfbf345f66a612e0cdfc4e35f69c75c0dd0
Rylie <rylie.stone2005@gmail.com>
2024-04-02 12:48:46 -0400

    articles now sends requests and brings the articles

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -1,16 +1,21 @@
 import React, { useEffect } from 'react'
-import { Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import PT from 'prop-types'
 
 export default function Articles(props) {
   // ✨ where are my props? Destructure them here
-
+  const navigate = useNavigate()
+  const {getArticles, articles, setCurrentArticle, deleteArticle } = props
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
+  if (!localStorage.getItem('token')) {
+    navigate('/')
+  }
 
   useEffect(() => {
+    getArticles()
     // ✨ grab the articles here, on first render only
-  })
+  }, [])
 
   return (
     // ✨ fix the JSX: replace `Function.prototype` with actual functions
@@ -18,9 +23,9 @@ export default function Articles(props) {
     <div className="articles">
       <h2>Articles</h2>
       {
-        ![].length
+        !articles.length
           ? 'No articles yet'
-          : [].map(art => {
+          : articles.map(art => {
             return (
               <div className="article" key={art.article_id}>
                 <div>
@@ -29,8 +34,8 @@ export default function Articles(props) {
                   <p>Topic: {art.topic}</p>
                 </div>
                 <div>
-                  <button disabled={true} onClick={Function.prototype}>Edit</button>
-                  <button disabled={true} onClick={Function.prototype}>Delete</button>
+                  <button onClick={() => setCurrentArticle(art)}>Edit</button>
+                  <button onClick={() => deleteArticle(art.article_id)}>Delete</button>
                 </div>
               </div>
             )
@@ -50,6 +55,6 @@ Articles.propTypes = {
   })).isRequired,
   getArticles: PT.func.isRequired,
   deleteArticle: PT.func.isRequired,
-  setCurrentArticleId: PT.func.isRequired,
+  setCurrentArticle: PT.func.isRequired,
   currentArticleId: PT.number, // can be undefined or null
 }
